39892df311080a671a074ff29c431c01c69de2e7
sakil <147094431+sakilrocks@users.noreply.github.com>
2025-09-21 20:54:28 +0530

    Create compressor.h

---
+++ include/compressor.h
@@ -0,0 +1,14 @@
+#ifndef COMPRESSOR_H
+#define COMPRESSOR_H
+
+#include <stddef.h>
+
+// compress input file into output file
+
+int compress_file(const char *input_filename, const char *output_filename);
+
+// decompress input file into output file
+
+int decompress_file(const char *input_filename, const char *output_filename);
+
+#endif
